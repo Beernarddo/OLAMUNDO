240c006cd70d6affa9a4558b5476ddc81ef0a193
beernarddo <beernardofontoura@gmail.com>
2021-08-21 22:15:00 -0300

    finalizar o programa olamundo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OLAMUNDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá usuário, eu me chamo Bernardo!");
+        }
+    }
+}
